67fd40d002b7d2c5284275b972b405741ed934b2
Hamody <ahmed.hamody5532@gmail.com>
2024-07-17 19:03:33 +0300

    add loader_id to database

---
+++ database/migrations/2024_07_17_155918_add_loader_id_to_individual_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('individual_transactions', function (Blueprint $table) {
+            //
+            $table->unsignedBigInteger('loader_id')->nullable(); // Add loader_id column
+
+            // Add foreign key constraint for loader_id
+            $table->foreign('loader_id')->references('id')->on('employees')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('individual_transactions', function (Blueprint $table) {
+            //
+            $table->dropForeign(['loader_id']);
+            $table->dropColumn(['cargo_type', 'loader_id']);
+        });
+    }
+};
