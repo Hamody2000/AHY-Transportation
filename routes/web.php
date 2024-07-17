10b406c17c04ed2e85ee40a65d8949c6387f37ce
Hamody <ahmed.hamody5532@gmail.com>
2024-07-17 15:22:16 +0300

    change invoice route name to match the blade again

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('employees/{employee}/loans', [LoanController::class, 'showEmployeeLoans'])->name('employees.loans');
     Route::get('/invoices/individual/{id}', [IndividualTransactionController::class, 'printInvoice'])->name('invoices.individual');
     Route::get('/invoices/company/{id}', [CompanyTransactionController::class, 'printInvoice'])->name('invoices.company');
-    Route::get('company-transactions/{id}/print-driver-invoice', [CompanyTransactionController::class, 'printDriverInvoice'])->name('invoices.driver');
+    Route::get('/invoices/company/{id}', [CompanyTransactionController::class, 'printDriverInvoice'])->name('invoices.driver');
 
     Route::get('clients/{id}/transactions', [ClientController::class, 'showTransactions'])->name('clients.transactions');
     Route::get('/transactions/search', [TransactionController::class, 'search'])->name('transactions.search');
